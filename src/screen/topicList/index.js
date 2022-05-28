2bb49ab2d2698aeb8f6369e7e44c8f4b8e780346
mani3710 <mani37105@gmail.com>
2022-05-28 15:24:08 +0530

    update : Toast message is added

--- src/screen/topicList/index.js
+++ src/screen/topicList/index.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getStudentDataListByStaffAndBatch, getProjectReviewList, getReviewTopicList, updateReviewMark } from '../../redux/reducers/project';
 import * as Routes from '../../navigation/routes';
 import { Button } from 'react-native-elements';
+import Toast from 'react-native-simple-toast';
 const TopicList = (props) => {
     const dispatch = useDispatch();
     const projectStore = useSelector(state => state.project);
@@ -32,10 +33,25 @@ const TopicList = (props) => {
         console.log("selectedReview", selectedReview);
         dispatch(getReviewTopicList(selectedReview.uuid));
     }
+    const submit = () => {
+        let flag = true;
+        for (let obj of topicList) {
+            if (!obj.mark) {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag) {
+            Toast.show("Empty Field Found !");
+            return;
+        }
+
+    }
     return (
         <View style={{ backgroundColor: "#fff", flex: 1 }}>
             <Text style={{ color: "black", fontWeight: "bold", marginLeft: 20, fontSize: 24, marginTop: 20 }}>{selectedStundent.username}</Text>
             <Text style={{ color: "#808080", fontSize: 13, marginLeft: 20, }}>{selectedStundent.rollno}</Text>
+            <Text style={{ color: "black", fontWeight: "bold", marginLeft: 20, fontSize: 24, marginTop: 5, alignSelf: "center" }}>{selectedReview.reviewname}</Text>
             <FlatList
                 data={topicList}
                 renderItem={({ item, index }) => {
@@ -56,6 +72,11 @@ const TopicList = (props) => {
                                     //  setStaffID(text) 
                                     if (parseInt(text) <= parseInt(item.maxmark)) {
                                         dispatch(updateReviewMark({ index: index, mark: text }));
+                                    } else if (!text) {
+                                        dispatch(updateReviewMark({ index: index, mark: "" }));
+                                    }
+                                    else {
+                                        Toast.show(`Max mark : ${item.maxmark}`);
                                     }
                                 }}
                                 style={{ borderWidth: 1, borderColor: "transparent", borderRadius: 10, borderBottomColor: "#A9A9A9", marginTop: 12, color: "#000" }}
@@ -69,7 +90,7 @@ const TopicList = (props) => {
                 title="UPDATE MARK"
                 containerStyle={{ backgroundColor: "#56121D", width: "80%", marginTop: 25, alignSelf: "center", marginBottom: 25 }}
                 onPress={() => {
-                    //  staffSignFunc()
+                    submit()
                 }}
             />
 
